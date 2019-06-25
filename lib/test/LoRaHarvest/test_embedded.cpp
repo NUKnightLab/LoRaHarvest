c19396128461da62b317b9c61abb4d632353ea0c
Scott Bradley <scottbradley.mail@gmail.com>
2019-06-25 11:22:01 -0500

    fixes route handling

--- lib/test/LoRaHarvest/test_embedded.cpp
+++ lib/test/LoRaHarvest/test_embedded.cpp
@@ -13,35 +13,29 @@ namespace Test_EmbeddedThings {
     }
 
     void test_handlePacket_SENDDATA(void) {
-        int to = NODE_ID;
+        uint8_t to = NODE_ID;
         int from = 0;
         int dest = NODE_ID;
         int type = PACKET_TYPE_SENDDATA;
-        // routes here are built as would come from a remote node
-        uint8_t route[2] = { (uint8_t)(NODE_ID+1), (uint8_t)NODE_ID };
-        println("first check size of route: %d", sizeof(route));
+        // routes here are built as would come from a remote node. ie, the current node id will be last
+        uint8_t route[2] = { NODE_ID+1, NODE_ID };
         uint8_t *message;
-        println("NEXT DATA PACKET");
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_SENT_NEXT_DATA_PACKET,
             handlePacket(to, from, dest, ++++sequence, type, route, 2, message));
-        println("DUPLICATE SEQUENCE");
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_DUPLICATE_SEQUENCE,
             handlePacket(to, from, dest, ++sequence, type, route, 2, message));
-        println("WRONG ADDRESS");
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_WRONG_ADDRESS,
             handlePacket(to+1, from, dest, ++sequence, type, route, 2, message));
-        println("NEXT DATA PACKET");
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_SENT_NEXT_DATA_PACKET,
             handlePacket(255, from, dest, ++++sequence, type, route, 2, message));
-        println("CODE NONE");
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_NONE,
             handlePacket(255, from, 255, ++++sequence, type, route, 2, message));
-        uint8_t route_multi[3] = { (uint8_t)(NODE_ID+2), (uint8_t)(NODE_ID+1), (uint8_t)(NODE_ID) };
+        uint8_t route_multi[3] = { NODE_ID+2, NODE_ID+1, NODE_ID };
         TEST_ASSERT_EQUAL(
             MESSAGE_CODE_SENT_NEXT_DATA_PACKET,
             handlePacket(255, from, dest, ++++sequence, type, route_multi, 3, message));
